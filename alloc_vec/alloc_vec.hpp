f2dfc606298b482b96668f48a54517b2b8896490
lirunjing-china <3189203910@qq.com>
2020-05-24 12:07:23 +0800

    rebuild

--- alloc_vec/alloc_vec.hpp
+++ alloc_vec/alloc_vec.hpp
@@ -9,25 +9,27 @@ private:
     int maxn=100,sizenow=0;
     std::allocator<T> alloc;
     T * a = alloc.allocate(maxn);
-    void update()
-    {
-        maxn *= 2;
-        T * b = alloc.allocate(maxn);
-        for (int i=0;i<sizenow;i++)
-        {
-            alloc.construct(b+i,*(a+i));
-        }
-        a = b;
-    }
+    void update();
 public:
-    void push_back(const T & item0)
-    {
-        if (sizenow==maxn-1)  update();
-        alloc.construct(a+sizenow,item0);
-        ++ sizenow;
-    }
+    void push_back(const T & item0);
     T operator [] (const int & n)
     {
         return *(a+n);
     }
 };
+template<class T>
+void alloc_vec<T>::update()
+{
+    maxn *= 2;
+    T * b = alloc.allocate(maxn);
+    for (int i=0;i<sizenow;++i)
+        alloc.construct(b+i,*(a+i));
+    a = b;
+}
+template<class T>
+void alloc_vec<T>::push_back(const T& item0)
+{
+    if (sizenow==maxn-1)    update();
+    alloc.construct(a+sizenow,item0);
+    ++ sizenow;
+}
